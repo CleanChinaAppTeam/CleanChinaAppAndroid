32722110754841b103d40848c8797adc58493fc4
Deng Jun <dengjun86@gmail.com>
2013-12-20 18:07:39 +0800

    meeting page

---
+++ CleanChina/src/com/cleanchina/meeting/MeetingActivity.java
@@ -0,0 +1,16 @@
+package com.cleanchina.meeting;
+
+import android.os.Bundle;
+
+import com.cleanchina.R;
+import com.cleanchina.app.CCActivity;
+
+public class MeetingActivity extends CCActivity {
+
+	@Override
+	protected void onCreate(Bundle arg0) {
+		super.onCreate(arg0);
+		setContentView(R.layout.activity_meeting);
+	}
+
+}
